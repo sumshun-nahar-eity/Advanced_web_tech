33b65dfd2b1263451e1dabed03f57d69b97326c2
sumshun-nahar-eity <esumshun@gmail.com>
2026-04-16 00:16:23 +0600

    rename lab 3

---
+++ university-system/src/enrollment/enrollment.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { EnrollmentService } from './enrollment.service';
+
+@Controller('enrollment')
+export class EnrollmentController {
+
+  constructor(private enrollmentService: EnrollmentService) {}
+
+  @Get()
+  getAll() {
+    return this.enrollmentService.getEnrollments();
+  }
+
+  @Post()
+  enroll(@Body() body: any) {
+    return this.enrollmentService.enrollStudent(
+      body.studentName,
+      body.courseId
+    );
+  }
+}
