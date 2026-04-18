5de22d1b52a9a9a72b78ab6296bf191f541aa716
sumshun-nahar-eity <139330047+sumshun-nahar-eity@users.noreply.github.com>
2026-04-18 21:09:03 +0600

    Delete university-system directory

--- university-system/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CourseModule } from './course/course.module';
-import { EnrollmentModule } from './enrollment/enrollment.module';
-import { NotificationModule } from './notification/notification.module';
-
-@Module({
-  imports: [CourseModule, EnrollmentModule, NotificationModule],
-})
-export class AppModule {}
